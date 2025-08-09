cf9efb63abaf303f8956294afe5d210665e709f4
dawon1205 <won394@naver.com>
2025-08-09 16:30:27 +0900

    SUMMER_Lab03

---
+++ SUMMER_Lab03_1.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	int arr[1001] = { 0 };
+	arr[1] = 1;
+	arr[2] = 3;
+	for (int i = 3;i <= n;i++) {
+		arr[i] = (arr[i - 1] + 2 * arr[i - 2]) % 10007;
+	}
+
+	printf("%d\n", arr[n]);
+
+	return 0;
+}
