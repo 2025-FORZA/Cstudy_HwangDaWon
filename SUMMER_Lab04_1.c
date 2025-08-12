3f35848d2c08497738d627f92246fe5a8206230d
dawon1205 <won394@naver.com>
2025-08-12 18:26:01 +0900

    SUMMER_Lab04

---
+++ SUMMER_Lab04_1.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int arr[9];
+	int n;
+	for (int i = 0;i <9;i++) {
+		scanf("%d", &n);
+		arr[i] = n;
+	}
+	int max = arr[0];
+	int index=1;
+	for (int i = 0;i <= 7;i++) {
+		if (max < arr[i+1]) {
+			max = arr[i + 1];
+			index = i + 2;
+		}
+	}
+	printf("%d\n%d", max,index);
+}
