72b41be77f51be6f1f3981da1eb0ce68ab198749
dawon1205 <won394@naver.com>
2025-11-19 16:44:10 +0900

    2_Lab09

---
+++ 2_Lab09_2.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int t, r;
+	char s[21];
+	scanf("%d", &t);
+	for (int i = 0;i < t;i++) {
+		scanf("%d %s", &r, s);
+		for (int j = 0;s[j] != '\0';j++) {
+			for (int k = 0;k < r;k++) {
+				printf("%c", s[j]);
+			}
+		}
+		printf("\n");
+	}
+}
